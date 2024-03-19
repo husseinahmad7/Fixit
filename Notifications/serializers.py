dab9a43130e31ca711b208f923ea282d917ab259
husseinahmad7 <h7osanna.xyc@gmail.com>
2024-03-19 10:55:56 +0300

    debug

--- Notifications/serializers.py
+++ Notifications/serializers.py
@@ -2,6 +2,9 @@
 from rest_framework import serializers
 from .models import Notification
 from Ticket.serializers import TicketSerializer
+from Ticket.decorators import query_debugger
+from django.utils.decorators import method_decorator
+@method_decorator(query_debugger,name='to_representation')
 class NotificationSerializer(serializers.ModelSerializer):
     ticket = TicketSerializer()
     title = serializers.SerializerMethodField()
