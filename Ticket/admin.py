1ae0afec2386d7333bfd885a088887fe9ff509e2
husseinahmad7 <h7osanna.xyc@gmail.com>
2024-03-28 11:29:51 +0300

    add notify by email action

--- Ticket/admin.py
+++ Ticket/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import ServiceCategory, Service, Ticket
+from django.core.mail import send_mail
 
 class ServiceCategoryAdmin(admin.ModelAdmin):
     list_display = ('title',)
@@ -16,7 +17,7 @@ class TicketAdmin(admin.ModelAdmin):
     list_filter = ('status', 'assigned_to')
     search_fields = ('client__email', 'service__title', 'description')
     ordering = ('-submission_date',)
-    readonly_fields = ('submission_date',)  # Mark submission_date as read-only
+    readonly_fields = ('submission_date',)
 
     fieldsets = (
         ('General Information', {
@@ -26,6 +27,27 @@ class TicketAdmin(admin.ModelAdmin):
             'fields': ('description', 'location', 'info_fields', 'client_rating', 'notes', 'final_price', 'workers', 'paycode')
         }),
     )
+
+    def send_notification_email(self, request, queryset):
+        """
+        Custom admin action to send email notifications to clients.
+        """
+        for ticket in queryset:
+            if ticket.client:
+                # Customize the email subject and content
+                subject = f"Ticket Update: Ticket #{ticket.id} Status Changed"
+                message = f"Dear {ticket.client.full_name},\n\nYour ticket status has been updated to {ticket.status}."
+                from_email = "admin@fixit.com"
+                recipient_list = [ticket.client.email]
+
+                # Send the email
+                send_mail(subject, message, from_email, recipient_list)
+
+        self.message_user(request, f"Notification emails sent to {queryset.count()} clients.")
+
+    send_notification_email.short_description = "Send notification email to clients"
+
+    actions = [send_notification_email]
 
 
 admin.site.register(ServiceCategory, ServiceCategoryAdmin)
