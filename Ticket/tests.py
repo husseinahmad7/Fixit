0939c4652027fb9e294182da637c8b056e48108a
husseinahmad7 <h7osanna.xyc@gmail.com>
2024-04-20 17:57:46 +0300

    fixs

--- Ticket/tests.py
+++ Ticket/tests.py
@@ -1,3 +1,158 @@
 from django.test import TestCase
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.urls import reverse
+from .models import ServiceCategory, Service, Ticket, TicketPicture
+from Users.models import User,Staff
 
-# Create your tests here.
+with open('tests/test_image.jpg', 'rb') as file:
+    data = file.read()
+
+# a SimpleUploadedFile instance
+uploaded_file = SimpleUploadedFile('test_image.jpg', data)
+
+class TestServiceCategory(TestCase):
+    def setUp(self):
+        self.category = ServiceCategory.objects.create(title='Test Category', icon=uploaded_file)
+
+    def test_str(self):
+        self.assertEqual(str(self.category), 'Test Category')
+
+    # def test_icon_upload(self):
+    #     self.assertEqual(self.category.icon.name, 'service_categories/test_image.jpg')
+
+class TestService(TestCase):
+    def setUp(self):
+        self.category = ServiceCategory.objects.create(title='Test Category', icon=uploaded_file)
+        self.service = Service.objects.create(title='Test Service',initial_price=3900, service_category=self.category, picture=uploaded_file)
+
+    def test_str(self):
+        self.assertEqual(str(self.service), 'Test Service')
+
+    # def test_picture_upload(self):
+    #     self.assertEqual(self.service.picture.name, 'serv_pictures/test_image.jpg')
+
+class TestTicket(TestCase):
+    def setUp(self):
+        self.client = User.objects.create_user(email='hhd@dhd.vb', password='test_password')
+        self.client2 = User.objects.create_user(email='eeeed@dhd.vb', password='test_password', is_staff=True)
+        self.staff = Staff.objects.create(user=self.client2,department='tech')
+
+        self.service = Service.objects.create(title='Test Service',initial_price=3900, service_category=ServiceCategory.objects.create(title='Test Category', icon=uploaded_file), picture=uploaded_file)
+        self.ticket = Ticket.objects.create(client=self.client, service=self.service, assigned_to=self.staff,location={"2":"6"})
+
+    def test_str(self):
+        self.assertEqual(str(self.ticket), f'{self.ticket.pk} -- Open')
+
+    def test_client_assignment(self):
+        self.assertEqual(self.ticket.client, self.client)
+
+    def test_service_assignment(self):
+        self.assertEqual(self.ticket.service, self.service)
+
+    def test_staff_assignment(self):
+        self.assertEqual(self.ticket.assigned_to, self.staff)
+
+    def test_status_default(self):
+        self.assertEqual(self.ticket.status, 'Open')
+
+# class TestTicketPicture(TestCase):
+#     def setUp(self):
+#         self.ticket = Ticket.objects.create(client=User.objects.create_user(username='test_client', password='test_password'), service=Service.objects.create(title='Test Service', service_category=ServiceCategory.objects.create(title='Test Category', icon=SimpleUploadedFile(open('tests/test_image.jpg', 'rb'))), picture=SimpleUploadedFile(open('tests/test_image.jpg', 'rb'))), assigned_to=User.objects.create_user(username='test_staff', password='test_password', is_staff=True))
+#         self.picture = SimpleUploadedFile(open('tests/test_image.jpg', 'rb'))
+#         self.ticket_picture = TicketPicture.objects.create(ticket=self.ticket, picture=self.picture)
+
+#     def test_ticket_assignment(self):
+#         self.assertEqual(self.ticket_picture.ticket, self.ticket)
+
+#     def test_picture_upload(self):
+#         self.assertEqual(self.ticket_picture.picture.name, 'attachments/test_image.jpg')
+
+# class TestDeleteOldImage(TestCase):
+#     def setUp(self):
+#         self.category = ServiceCategory.objects.create(title='Test Category', icon=SimpleUploadedFile(open('tests/test_image.jpg', 'rb')))
+#         self.service = Service.objects.create(title='Test Service', service_category=self.category, picture=SimpleUploadedFile(open('tests/test_image.jpg', 'rb')))
+#         self.ticket = Ticket.objects.create(client=User.objects.create_user(username='test_client', password='test_password'), service=self.service, assigned_to=User.objects.create_user(username='test_staff', password='test_password', is_staff=True))
+#         self.picture = SimpleUploadedFile(open('tests/test_image.jpg', 'rb'))
+#         self.ticket_picture = TicketPicture.objects.create(ticket=self.ticket, picture=self.picture)
+
+#     def test_delete_old_image(self):
+#         old_image = self.ticket_picture.picture
+#         new_image = SimpleUploadedFile(open('tests/test_image.jpg', 'rb'))
+#         self.ticket_picture.picture = new_image
+#         self.ticket_picture.save()
+#         self.assertFalse(default_storage.exists(old_image.name))
+
+#     def test_upload_new_image(self):
+#         new_image = SimpleUploadedFile(open('tests/test_image.jpg', 'rb'))
+#         self.ticket_picture.picture = new_image
+#         self.ticket_picture.save()
+#         self.assertTrue(default_storage.exists(self.ticket_picture.picture.name))
+
+class TestTicketChangeStatus(TestCase):
+    def setUp(self):
+        self.client = User.objects.create_user(email='hhd@dhd.vb', password='test_password')
+        self.client2 = User.objects.create_user(email='eeehhd@dhd.vb', password='test_password', is_staff=True)
+        self.staff = Staff.objects.create(user=self.client2,department='tech')
+        self.service = Service.objects.create(title='Test Service', initial_price=3900, service_category=ServiceCategory.objects.create(title='Test Category', icon=uploaded_file), picture=uploaded_file)
+        self.ticket = Ticket.objects.create(client=self.client, service=self.service, assigned_to=self.staff,location={"2":"6"})
+
+    def test_status_change(self):
+        self.assertEqual(self.ticket.status, 'Open')
+        self.ticket.status = 'In Progress'
+        self.ticket.save()
+        self.assertEqual(self.ticket.status, 'In Progress')
+
+# class TestTicketAddNote(TestCase):
+#     def setUp(self):
+#         self.client = User.objects.create_user(username='test_client', password='test_password')
+#         self.staff = User.objects.create_user(username='test_staff', password='test_password', is_staff=True)
+#         self.service = Service.objects.create(title='Test Service', service_category=ServiceCategory.objects.create(title='Test Category', icon=SimpleUploadedFile(open('tests/test_image.jpg', 'rb'))), picture=SimpleUploadedFile(open('tests/test_image.jpg', 'rb')))
+#         self.ticket = Ticket.objects.create(client=self.client, service=self.service, assigned_to=self.staff)
+#         self.note = 'This is a test note.'
+#         self.ticket.add_note(self.staff, self.note)
+
+#     def test_note_added(self):
+#         self.assertEqual(self.ticket.notes.latest('pk').note, self.note)
+
+#     def test_note_user(self):
+#         self.assertEqual(self.ticket.notes.latest('pk').user, self.staff)
+
+#     def test_note_ticket(self):
+#         self.assertEqual(self.ticket.notes.latest('pk').ticket, self.ticket)
+
+# class TestTicketClose(TestCase):
+#     def setUp(self):
+#         self.client = User.objects.create_user(username='test_client', password='test_password')
+#         self.staff = User.objects.create_user(username='test_staff', password='test_password', is_staff=True)
+#         self.service = Service.objects.create(title='Test Service', service_category=ServiceCategory.objects.create(title='Test Category', icon=SimpleUploadedFile(open('tests/test_image.jpg', 'rb'))), picture=SimpleUploadedFile(open('tests/test_image.jpg', 'rb')))
+#         self.ticket = Ticket.objects.create(client=self.client, service=self.service, assigned_to=self.staff)
+#         self.note = 'This is a test note.'
+#         self.ticket.add_note(self.staff, self.note)
+
+#     def test_close_ticket(self):
+#         self.assertEqual(self.ticket.status, 'Open')
+#         self.ticket.close()
+#         self.assertEqual(self.ticket.status, 'Closed')
+
+#     def test_close_ticket_note(self):
+#         self.ticket.close()
+#         self.assertEqual(self.ticket.notes.latest('pk').note, 'Ticket closed by {0}.'.format(self.staff.username))
+
+# class TestTicketReopen(TestCase):
+#     def setUp(self):
+#         self.client = User.objects.create_user(username='test_client', password='test_password')
+#         self.staff = User.objects.create_user(username='test_staff', password='test_password', is_staff=True)
+#         self.service = Service.objects.create(title='Test Service', service_category=ServiceCategory.objects.create(title='Test Category', icon=SimpleUploadedFile(open('tests/test_image.jpg', 'rb'))), picture=SimpleUploadedFile(open('tests/test_image.jpg', 'rb')))
+#         self.ticket = Ticket.objects.create(client=self.client, service=self.service, assigned_to=self.staff)
+#         self.note = 'This is a test note.'
+#         self.ticket.add_note(self.staff, self.note)
+#         self.ticket.close()
+
+#     def test_reopen_ticket(self):
+#         self.assertEqual(self.ticket.status, 'Closed')
+#         self.ticket.reopen()
+#         self.assertEqual(self.ticket.status, 'Open')
+
+#     def test_reopen_ticket_note(self):
+#         self.ticket.reopen()
+#         self.assertEqual(self.ticket.notes.latest('pk').note, 'Ticket reopened by {0}.'.format(self.staff.username))
--- Users/tests.py
+++ Users/tests.py
@@ -1,20 +1,255 @@
-from django.test import TestCase
-from .models import User
+from django.test import TestCase, Client
+from rest_framework.test import APIRequestFactory
+from django.urls import reverse
+from django.contrib.auth.models import User as AuthUser
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.contrib.auth.models import Group
+from .models import User, Staff
+from Ticket.models import Service, ServiceCategory
 
-# Create your tests here.
-from rest_framework.test import APIClient
-from rest_framework.authtoken.models import Token
 
-class ExampleTestCase(TestCase):
+with open('tests/test_image.jpg', 'rb') as file:
+    data = file.read()
+
+# a SimpleUploadedFile instance
+uploaded_file = SimpleUploadedFile('test_image.jpg', data)
+
+class UserModelTests(TestCase):
+
     def setUp(self):
-        self.client = APIClient()
         self.user = User.objects.create_user(
-            username='hussein',
-            password='11qq22ww'
+            email='testuser@example.com',
+            password='testpassword',
+            full_name='Test User',
+            mobile='1234567890',
         )
-        self.token = Token.objects.create(user=self.user)
 
-    def test_example_view(self):
-        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
-        response = self.client.post('/logout/')
-        self.assertEqual(response.status_code, 200)
+    def test_user_str(self):
+        self.assertEqual(str(self.user), 'Test User')
+
+    def test_user_email(self):
+        self.assertEqual(self.user.email, 'testuser@example.com')
+
+    def test_user_full_name(self):
+        self.assertEqual(self.user.full_name, 'Test User')
+
+    def test_user_mobile(self):
+        self.assertEqual(self.user.mobile, '1234567890')
+
+    def test_user_is_staff(self):
+        self.assertEqual(self.user.is_staff, False)
+
+    def test_user_is_superuser(self):
+        self.assertEqual(self.user.is_superuser, False)
+
+    def test_user_groups(self):
+        self.assertEqual(len(self.user.groups.all()), 0)
+
+    def test_user_permissions(self):
+        self.assertEqual(len(self.user.user_permissions.all()), 0)
+
+    def test_user_is_active(self):
+        self.assertEqual(self.user.is_active, True)
+
+    # def test_user_username(self):
+    #     self.assertEqual(self.user.username, 'testuser@example.com')
+
+    def test_user_full_name(self):
+        self.assertEqual(self.user.full_name, 'Test User')
+
+    def test_user_mobile(self):
+        self.assertEqual(self.user.mobile, '1234567890')
+
+    def test_user_device_reg_id(self):
+        self.assertEqual(self.user.device_reg_id, None)
+
+class StaffModelTests(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            email='testuser@example.com',
+            password='testpassword',
+            full_name='Test User',
+            mobile='1234567890',
+        )
+        self.staff = Staff.objects.create(
+            user=self.user,
+            department='IT',
+            salary=1000,
+            availability=True,
+            is_supervisor=False,
+        )
+
+    def test_staff_str(self):
+        self.assertEqual(str(self.staff), 'Test User')
+
+    def test_staff_user(self):
+        self.assertEqual(self.staff.user, self.user)
+
+    def test_staff_department(self):
+        self.assertEqual(self.staff.department, 'IT')
+
+    def test_staff_salary(self):
+        self.assertEqual(self.staff.salary, 1000)
+
+    def test_staff_availability(self):
+        self.assertEqual(self.staff.availability, True)
+
+    def test_staff_services(self):
+        self.assertEqual(len(self.staff.services.all()), 0)
+
+    def test_staff_is_supervisor(self):
+        self.assertEqual(self.staff.is_supervisor, False)
+
+    def test_staff_get_absolute_url(self):
+        self.assertEqual(self.staff.get_absolute_url(), f'/st/staff/{self.staff.pk}/')
+
+
+
+# class TestViews(TestCase):
+#     def setUp(self):
+#         self.client = Client()
+#         self.user = User.objects.create_user(
+#             email='test@example.com',
+#             password='testpassword',
+#             full_name='Test User',
+#             mobile='1234567890',
+#         )
+#         self.staff = Staff.objects.create(
+#             user=self.user,
+#             department='IT',
+#             salary=1000,
+#             availability=True,
+#             is_supervisor=False,
+#         )
+#         # self.cat =ServiceCategory.objects.create(title='Category 1')
+#         self.service = Service.objects.create(title='Test Service',initial_price=3000, service_category=ServiceCategory.objects.create(title='Test Category', icon=uploaded_file), picture=uploaded_file)
+
+#         # self.service = Service.objects.create(title='Service 1',)
+#         self.api_client = APIRequestFactory()
+
+#     def test_users_list_create_view(self):
+#         self.client.force_login(self.user)
+#         response = self.client.get(reverse('users:list'))
+#         self.assertEqual(response.status_code, 200)
+
+#         response = self.client.post(reverse('users:list'), {
+#             'email': 'newuser@example.com',
+#             'password': 'newpassword',
+#             'full_name': 'New User',
+#             'mobile': '0987654321',
+#         })
+#         self.assertEqual(response.status_code, 201)
+#         self.assertEqual(User.objects.count(), 2)
+
+#     def test_users_retrieve_delete_update_view(self):
+#         self.client.force_login(self.user)
+#         response = self.client.get(reverse('users:retrieve-update-delete', args=[self.user.pk]))
+#         self.assertEqual(response.status_code, 200)
+
+#         response = self.client.put(reverse('users:retrieve-update-delete', args=[self.user.pk]), {
+#             'email': 'updateduser@example.com',
+#             'password': 'updatedpassword',
+#             'full_name': 'Updated User',
+#             'mobile': '1111111111',
+#         })
+#         self.assertEqual(response.status_code, 200)
+#         self.user.refresh_from_db()
+#         self.assertEqual(self.user.email, 'updateduser@example.com')
+
+#         response = self.client.delete(reverse('users:retrieve-update-delete', args=[self.user.pk]))
+#         self.assertEqual(response.status_code, 204)
+#         self.assertEqual(User.objects.count(), 1)
+
+#     def test_user_retrieve_update_api_view(self):
+#         self.client.force_login(self.user)
+#         request = self.api_client.get(reverse('users:retrieve-update', args=[self.user.pk]))
+#         self.assertEqual(request.status_code, 200)
+
+#         request = self.api_client.put(reverse('users:retrieve-update', args=[self.user.pk]), {
+#             'email': 'updateduser@example.com',
+#             'password': 'updatedpassword',
+#             'full_name': 'Updated User',
+#             'mobile': '1111111111',
+#         })
+#         self.assertEqual(request.status_code, 200)
+#         self.user.refresh_from_db()
+#         self.assertEqual(self.user.email, 'updateduser@example.com')
+
+#     def test_staff_list_api_view(self):
+#         self.client.force_login(self.user)
+#         response = self.client.get(reverse('users:staff-list'))
+#         self.assertEqual(response.status_code, 200)
+
+#     def test_staffs_retrieve_delete_update_view(self):
+#         self.client.force_login(self.user)
+#         response = self.client.get(reverse('users:staff-retrieve-update-delete', args=[self.staff.pk]))
+#         self.assertEqual(response.status_code, 200)
+
+#         response = self.client.put(reverse('users:staff-retrieve-update-delete', args=[self.staff.pk]), {
+#             'department': 'Updated Department',
+#             'salary': 1001,
+#             'availability': False,
+#             'is_supervisor': True,
+#         })
+#         self.assertEqual(response.status_code, 200)
+#         self.staff.refresh_from_db()
+#         self.assertEqual(self.staff.department, 'Updated Department')
+
+#         response = self.client.delete(reverse('users:staff-retrieve-update-delete', args=[self.staff.pk]))
+#         self.assertEqual(response.status_code, 204)
+#         self.assertEqual(Staff.objects.count(), 0)
+
+#     def test_add_service_for_staff_view(self):
+#         self.client.force_login(self.user)
+#         response = self.client.post(reverse('users:add-service', args=[self.staff.pk]), {
+#             'service': self.service.pk,
+#         })
+#         self.assertEqual(response.status_code, 201)
+#         self.assertEqual(self.staff.services.count(), 1)
+
+#     def test_delete_service_for_staff_view(self):
+#         self.client.force_login(self.user)
+#         self.staff.services.add(self.service)
+#         response = self.client.delete(reverse('users:delete-service', args=[self.staff.pk, self.service.pk]))
+#         self.assertEqual(response.status_code, 204)
+#         self.assertEqual(self.staff.services.count(), 0)
+
+#     def test_user_registration_api_view(self):
+#         request = self.api_client.post(reverse('register'), {
+#             'email': 'newuser@example.com',
+#             'password': 'newpassword',
+#             'full_name': 'New User',
+#             'mobile': '0987654321',
+#         })
+#         self.assertEqual(request.status_code, 201)
+#         self.assertEqual(User.objects.count(), 2)
+
+#     def test_activate_user_api_view(self):
+#         request = self.api_client.post(reverse('register-activate'), {
+#             'activation_code': 'activationcode',
+#         })
+#         self.assertEqual(request.status_code, 200)
+
+#     def test_staff_registration_view(self):
+#         self.client.force_login(self.user)
+#         response = self.client.post(reverse('staff-registration'), {
+#             'department': 'IT',
+#             'salary': 1000,
+#             'availability': True,
+#             'is_supervisor': False,
+#         })
+#         self.assertEqual(response.status_code, 201)
+#         self.assertEqual(Staff.objects.count(), 2)
+
+#     def test_login_view(self):
+#         response = self.client.post(reverse('users:login'), {
+#             'email': 'test@example.com',
+#             'password': 'testpassword',
+#         })
+#         self.assertEqual(response.status_code, 200)
+
+#     def test_logout_view(self):
+#         self.client.force_login(self.user)
+#         response = self.client.post(reverse('users:logout'))
+#         self.assertEqual(response.status_code, 204)
